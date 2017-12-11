4e1393cf590cb2675fcad9c34b50c139c70aef04
taktazit <taktazit@outlook.com>
2017-12-11 15:49:05 +0330

    Initial commit.

---
+++ src/main/java/com/taktazit/dc/blockchian/FluctuationAnalyzer.java
@@ -0,0 +1,11 @@
+package com.taktazit.dc.blockchian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluctuationAnalyzer {
+    public static void main(String ... args){
+        SpringApplication.run(FluctuationAnalyzer.class, args);
+    }
+}
---
+++ src/main/java/com/taktazit/dc/blockchian/client/TransactionClient.java
@@ -0,0 +1,4 @@
+package com.taktazit.dc.blockchian.client;
+
+public interface TransactionClient {
+}
